c1d3fcf54337288b52d3ced955ae80a0584ac94f
Yash saini <yashsaini4824@gmail.com>
2026-01-13 21:50:53 +0530

    Add camel casing guideline comment
    Added a comment about camel casing in variable names.

--- basic_dart_codes.dart
+++ basic_dart_codes.dart
@@ -4,3 +4,11 @@ void main() {
   String msg = "Hello world"; // here S of string should be capital
   print(msg.length);
 }
+
+/* we should always use camel casing while writing our codes 
+for example 
+
+  String someValue = "Hello world"; 
+
+here someValue is ex of camel casing
+
