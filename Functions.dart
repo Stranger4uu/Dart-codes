7687427282e34d6b43039cfa6f1b80f35882efbe
Stranger4uu <yashsaini4824@gmail.com>
2026-01-23 09:48:46 +0530

    completed functions for now , will be updating this file in future

--- Functions.dart
+++ Functions.dart
@@ -25,7 +25,7 @@ String hello_world(){
 */
 // There are some examples of function , we can use any data type 
 // void does not return anything , not even null 
-
+/*
 void main(){
   String name = "Yash here"; // Local variable
   printName(name);
@@ -33,3 +33,20 @@ void main(){
 void printName(String name){
   print(name);
 }
+*/
+
+void main(){
+  String name = "Hello";
+  print_name(name: name, age : 19 , msg : "hello..." ); // whatever entered b/w () is called parameters 
+}
+                // whatever entered b/w {} is called arguments and currently it's positional argument but we can also add named arguments too
+void print_name({required String name, int? age, required String msg}){ // here int? means null value , so if user doesn't give any value it's fine
+//void print_name(int age,{required String name, int? age, required String msg}) // but we can add named arguments only right before positional arguments and not after 
+  print(name);
+  print(age);
+}
+//anonymous functions
+
+// eg(){
+//   print('hey');
+// }
