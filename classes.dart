d98c96257ca1c10eb9b999a23915466f5fe4a970
Stranger4uu <yashsaini4824@gmail.com>
2026-01-25 11:11:37 +0530

    updated classes file

--- classes.dart
+++ classes.dart
@@ -1,6 +1,7 @@
 // classed are like blueprints for creating objects ( in class we use "PascalCase" like this )
 // A function in a class is called "Method"
 
+/*
 
 void main(){
   // Cookie(); // instantiated ( creating an object from a class )
@@ -22,6 +23,47 @@ class Cookie{
   }
 
   bool isCooling(){
+    return false;
+  }
+}
+*/
+
+void main(){
+  final cookie = Cookie(shape: 'circle',size : 12);
+  print(cookie.height);
+  cookie.setHeight = 15;
+  print(cookie.height);
+}
+
+class Cookie {
+  final String shape;
+  final double size;
+
+  // private variables
+  int _height = 4;
+  int _width = 5;
+
+  Cookie({required this.shape, required this.size}) {
+    baking();
+  }
+
+  // getter
+  int get height => _height;
+
+  // setter
+  set setHeight(int h) {
+    _height = h;
+  }
+
+  int calculateSize() {
+    return _height * _width;
+  }
+
+  void baking() {
+    print('Your cookie of $shape shape of $size size is cooking');
+  }
+
+  bool isCooling() {
     return false;
   }
 }
